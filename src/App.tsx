079f54a80be845bc0662e6fcdc31936830280908
matwming <matwming114@gmail.com>
2019-11-16 17:29:17 +1100

    fixed eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import RootNavigation from "./Navigation/rootNavigation";
 
--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const About: React.FC = () => {
   return <div>this is an about page</div>;
